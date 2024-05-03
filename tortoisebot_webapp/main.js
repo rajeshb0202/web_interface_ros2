0efa50fb8ba753d0198e441b25d1705285a08895
rajesh <rajeshb0202@gmail.com>
2024-05-03 01:58:51 +0000

    added the initial files and corrected the connection status block.

---
+++ tortoisebot_webapp/main.js
@@ -0,0 +1,40 @@
+var app = new Vue({
+    el: '#app',
+    // storing the state of the page
+    data: {
+        connected: false,
+        ros: null,
+        logs: [],
+        loading: false,
+        rosbridge_address: 'wss://i-09405a2d85d86dbd8.robotigniteacademy.com/4575e79a-8201-4432-959a-f1c77a317c6e/rosbridge/',
+        port: '9090',
+    },
+    // helper methods to connect to ROS
+    methods: {
+        connect: function() {
+            this.loading = true
+            this.ros = new ROSLIB.Ros({
+                url: this.rosbridge_address
+            })
+            this.ros.on('connection', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
+                this.connected = true
+                this.loading = false
+            })
+            this.ros.on('error', (error) => {
+                this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
+            })
+            this.ros.on('close', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
+                this.connected = false
+                this.loading = false
+            })
+        },
+        disconnect: function() {
+            this.ros.close()
+        },
+    },
+    mounted() {
+        console.log("page is ready!")
+    },
+})
